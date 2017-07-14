e4ecaaced92097b613e12068ea7185224472dbdc
Yun Fang <yun.fang@oracle.com>
2017-07-14 19:51:27 +0800

    init

---
+++ user/themes/thy/js/carousels.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#bx5').bxSlider({
+        minSlides: 5,
+        maxSlides: 5,
+        slideWidth: 780,
+        slideMargin: 1,
+        pager: false,
+        ticker: true,
+        speed: 24000,
+        tickerHover: true,
+        useCSS: false,
+        captions: true
+    });
+});
